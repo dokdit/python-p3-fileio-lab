af235eb719bb11b26ad55e7ac07489fc8205a8d2
Akuei <akueidok911@gmail.com>
2023-07-20 09:10:45 -0400

    Lab completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
+import os
+
 def write_file(file_name, file_content):
-    pass
+    full_path = os.path.join('/home/dongkuei/Desktop/flatIron/phase03/python-p3-fileio-lab/lib/', f'{file_name}.txt')
+    with open(full_path, mode='w', encoding='utf-8') as file_obj:
+        file_obj.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    full_path = os.path.join('/home/dongkuei/Desktop/flatIron/phase03/python-p3-fileio-lab/lib/', f'{file_name}.txt')
+    with open(full_path, mode='a', encoding='utf-8') as file_obj:
+        file_obj.write(append_content)
+
+import os
 
 def read_file(file_name):
-    pass
+    full_path = os.path.join('/home/dongkuei/Desktop/flatIron/phase03/python-p3-fileio-lab/lib/', f'{file_name}.txt')
+    with open(full_path, encoding='utf-8') as file_obj:
+        content = file_obj.read()  # Read the entire content of the file
+    return content
